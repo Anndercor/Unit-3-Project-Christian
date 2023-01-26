cfa908997a7dab328f67cce86b2d10e57be5be6e
Anndercor <123668070+Anndercor@users.noreply.github.com>
2023-01-26 10:54:16 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,50 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let storypart2Imalwaysready  = document.querySelector(".option-one-screen");
+
+let storypart2Maybelater  = document.querySelector(".option-two-screen");
+
+let storypart3Imalwaysready  = document.querySelector(".option-one-end");
+
+let storypart3Maybelater  = document.querySelector(".option-two-end");
+
+let buttom1 = document.querySelector(".option-one");
+let buttom2 =document.querySelector(".option-two");
+let buttomend = document.querySelector(".option-two-end");
+
+
+// When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables! //
+
+//
+buttom1.onclick=function(){
+storypart2Imalwaysready.style.display="block" ;
+};
+
+//
+//
+buttom2.onclick=function(){
+storypart2Maybelater.style.display="block" ;
+
+};
+//
+buttom2.onclick=function(){
+storypart2Maybelater.style.display="block" ;
+
+};
+//
+/*
+//
+buttom1end.onclick=function(){
+storypart3Imalwaysready.style.display="block" ;
+buttom1end.style.display="none";
+    storypart3Imalwaysready.style.display="none";
+};
+//
+buttom2end.onclick=function(){
+storypart3Maybelater  .style.display="block" ;
+    buttom2end.style.display="none";
+  storypart3Maybelater.style.display="none";
+};
+//
+
+*/
